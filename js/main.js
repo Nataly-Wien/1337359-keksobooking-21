52c7006fce6c03406f26c21d5d4fe396e0477dbf
Nataly <nataly-wien@yandex.ru>
2020-09-27 13:35:10 +0200

    Исправляет замечания при добавлении меток на карту

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ const CHECK_IN = 12;
 const CHECK_OUT = 14;
 const LOCATION_Y_MIN = 130;
 const LOCATION_Y_MAX = 630;
-const LOCATION_X_MIN = 50;
-const LOCATION_X_MAX = 1150;
+const LOCATION_X_MIN = 0;
+// const LOCATION_X_MAX = 1150;
 
 const TYPE_LIST = [
   `palace`,
@@ -36,13 +36,14 @@ const PHOTOS_ADDRESS = `http://o0.github.io/assets/images/tokyo/hotel1.jpg, `;
 const map = document.querySelector(`.map`);
 const template = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 const mapPins = map.querySelector(`.map__pins`);
+const locationXMax = mapPins.clientWidth;
 
 
 const getRandomInRange = (max, min = 1) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getRandomFromList = (list) => list[getRandomInRange(list.length - 1, 0)];
 
-const getRandomStringFromList = (list) => list.reduce((a, b) => a + (Math.random() > 0.5 ? `` : b + `, `), ``).slice(0, -2);
+const getRandomArrayFromList = (list) => list.filter((item) => item && Math.random() > 0.5);
 
 const getRandomPhotosArray = () => {
   const arr = [];
@@ -58,30 +59,30 @@ const getNoticesList = () => {
   const notices = [];
 
   for (let i = 1; i <= NOTICES_NUMBER; i++) {
-    const notice = {
+    const locX = getRandomInRange(locationXMax, LOCATION_X_MIN);
+    const locY = getRandomInRange(LOCATION_Y_MAX, LOCATION_Y_MIN);
+    notices.push({
       author: {
-        avatar: `img/avatars/user0${i}.png`
+        avatar: `img/avatars/user0${i}.png`,
       },
       location: {
-        x: getRandomInRange(LOCATION_X_MAX, LOCATION_X_MIN),
-        y: getRandomInRange(LOCATION_Y_MAX, LOCATION_Y_MIN),
+        x: locX,
+        y: locY,
       },
       offer: {
         title: `Заголовок предложения ${i}`,
+        address: `${locX}, ${locY}`,
         price: getRandomInRange(MAX_RANDOM) * PRICE_FACTOR,
         type: getRandomFromList(TYPE_LIST),
         rooms: getRandomInRange(MAX_ROOMS_NUMBER),
         guests: getRandomInRange(MAX_GUESTS_NUMBER),
-        checkin: `${getRandomInRange(CHECK_OUT, CHECK_IN)}: 00`,
-        checkout: `${getRandomInRange(CHECK_OUT, CHECK_IN)}: 00`,
-        features: getRandomStringFromList(FEATURES_LIST),
+        checkin: `${getRandomInRange(CHECK_OUT, CHECK_IN)}:00`,
+        checkout: `${getRandomInRange(CHECK_OUT, CHECK_IN)}:00`,
+        features: getRandomArrayFromList(FEATURES_LIST),
         description: `строка с описанием `.repeat(getRandomInRange(MAX_RANDOM)),
         photos: getRandomPhotosArray(),
       }
-    };
-    notice.offer.address = `${notice.location.x}, ${notice.location.y}`;
-
-    notices.push(notice);
+    });
   }
 
   return notices;
@@ -91,8 +92,8 @@ const generatePin = (notice) => {
   const pin = template.cloneNode(true);
   pin.querySelector(`img`).src = notice.author.avatar;
   pin.querySelector(`img`).alt = notice.offer.title;
-  pin.style.left = notice.location.x + `px`;
-  pin.style.top = notice.location.y + `px`;
+  pin.style.left = `${notice.location.x}px`;
+  pin.style.top = `${notice.location.y}px`;
 
   return pin;
 };
@@ -110,12 +111,13 @@ const getPinBlock = () => {
 
 const setPinsOffset = () => {
   const pins = mapPins.querySelectorAll(`.map__pin:not(.map__pin--main)`);
-  const pinStyle = window.getComputedStyle(pins[0], `::after`);
+  const pinStyle = window.getComputedStyle(pins[0]);
 
   pins.forEach((element) => {
-    element.style.left = Number(element.style.left.slice(0, -2)) - Number(pinStyle.width.slice(0, -2)) / 2 + `px`;
-    element.style.top = Number(element.style.top.slice(0, -2)) - Number(pinStyle.height.slice(0, -2)) + `px`;
+    element.style.left = `${parseInt(element.style.left, 10) - parseInt(pinStyle.width, 10) / 2}px`;
+    element.style.top = `${parseInt(element.style.top, 10) - parseInt(pinStyle.height, 10)}px`;
   });
+
 };
 
 
