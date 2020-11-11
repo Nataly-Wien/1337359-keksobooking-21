6be44cee6e1d6b10d03ffc1f9d9a3f3c85c9e9d8
Nataly <nataly-wien@yandex.ru>
2020-11-11 08:18:00 +0100

    Изменяет вставку фото и фич в каточку

--- js/card.js
+++ js/card.js
@@ -36,51 +36,76 @@ const getNoticeCard = (notice) => {
     }
   } = notice;
 
+  // Этот блок заменяет все, что ниже, до строки 108 включительно, это было заменчание проверяющего по innerHTML и по трудночитаемому куску 98-106
+
+  // const cardTemplate = document.querySelector(`#card`).content;
+  // const card = cardTemplate.querySelector(`.map__card`).cloneNode(true);
+  // const featuresBlock = card.querySelector(`.popup__features`);
+  // const photosBlock = card.querySelector(`.popup__photos`);
+  // const photoTemplate = photosBlock.querySelector(`.popup__photo`);
+  // const avatarBlock = card.querySelector(`.popup__avatar`);
+
+  // if (Object.keys(notice.offer).length === 0) {
+  //   card.hidden = true;
+  //   return card;
+  // }
+
+  // const roomsText = rooms ? `${rooms} ${window.utils.getWordForm(rooms, ROOM_FORMS)} ` : ``;
+  // const guestsText = guests ? (rooms ? `для` : `Для`) + ` ${guests} ${window.utils.getWordForm(guests, GUEST_FORMS)}` : ``;
+  // const checkinText = checkin ? `Заезд\u00a0после ${checkin}` : ``;
+  // const checkoutText = checkout ? (checkin ? `, выезд` : `Выезд`) + `\u00a0до ${checkout}` : ``;
+
+  // card.querySelector(`.popup__title`).textContent = title || ``;
+  // card.querySelector(`.popup__text--address`).textContent = address || ``;
+  // card.querySelector(`.popup__text--price`).textContent = price ? `${price}₽/ночь` : ``;
+  // card.querySelector(`.popup__type`).textContent = type ? OFFER_TYPE[type] : ``;
+  // card.querySelector(`.popup__description`).textContent = description || ``;
+  // card.querySelector(`.popup__text--capacity`).textContent = roomsText + guestsText;
+  // card.querySelector(`.popup__text--time`).textContent = checkinText + checkoutText;
+
+  // featuresBlock.innerHTML = ``;
+
+  // if (features) {
+  //   features.forEach((item) => {
+  //     const node = document.createElement(`li`);
+  //     node.classList.add(`popup__feature`, `popup__feature--${item}`);
+  //     featuresBlock.appendChild(node);
+  //   });
+  // }
+
+  // if (photos) {
+  //   photos.forEach((photo) => {
+  //     const node = photoTemplate.cloneNode();
+  //     node.src = photo;
+  //     photosBlock.appendChild(node);
+  //   });
+  // }
+
+  // photoTemplate.remove();
+
   const cardTemplate = document.querySelector(`#card`).content;
   const card = cardTemplate.querySelector(`.map__card`).cloneNode(true);
-  const featuresBlock = card.querySelector(`.popup__features`);
-  const photosBlock = card.querySelector(`.popup__photos`);
-  const photoTemplate = photosBlock.querySelector(`.popup__photo`);
-  const avatarBlock = card.querySelector(`.popup__avatar`);
 
   if (Object.keys(notice.offer).length === 0) {
     card.hidden = true;
     return card;
   }
 
-  const roomsText = rooms ? `${rooms} ${window.utils.getWordForm(rooms, ROOM_FORMS)} ` : ``;
-  const guestsText = guests ? (rooms ? `для` : `Для`) + ` ${guests} ${window.utils.getWordForm(guests, GUEST_FORMS)}` : ``;
-  const checkinText = checkin ? `Заезд\u00a0после ${checkin}` : ``;
-  const checkoutText = checkout ? (checkin ? `, выезд` : `Выезд`) + `\u00a0до ${checkout}` : ``;
-
   card.querySelector(`.popup__title`).textContent = title || ``;
   card.querySelector(`.popup__text--address`).textContent = address || ``;
   card.querySelector(`.popup__text--price`).textContent = price ? `${price}₽/ночь` : ``;
   card.querySelector(`.popup__type`).textContent = type ? OFFER_TYPE[type] : ``;
+  card.querySelector(`.popup__text--capacity`).textContent = (rooms ? `${rooms} ${window.utils.getWordForm(rooms, ROOM_FORMS)}` : ``) +
+    (guests ? ` для ${guests} ${window.utils.getWordForm(guests, GUEST_FORMS)}` : ``);
+  card.querySelector(`.popup__text--time`).textContent = (checkin ?
+    `Заезд\u00a0после ${checkin}` : ``) + (checkin && checkout ? `, ` : ``) + (checkout ? `выезд\u00a0до ${checkout}` : ``);
+  card.querySelector(`.popup__features`).innerHTML = features && features.length ?
+    features.reduce((string, item) => string + `<li class="popup__feature popup__feature--${item}"></li>`, ``) : ``;
   card.querySelector(`.popup__description`).textContent = description || ``;
-  card.querySelector(`.popup__text--capacity`).textContent = roomsText + guestsText;
-  card.querySelector(`.popup__text--time`).textContent = checkinText + checkoutText;
+  card.querySelector(`.popup__photos`).innerHTML = photos && photos.length ? photos.reduce((string, item) =>
+    string + `<img src="${item}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`, ``) : ``;
 
-  featuresBlock.innerHTML = ``;
-
-  if (features) {
-    features.forEach((item) => {
-      const node = document.createElement(`li`);
-      node.classList.add(`popup__feature`, `popup__feature--${item}`);
-      featuresBlock.appendChild(node);
-    });
-  }
-
-  if (photos) {
-    photos.forEach((photo) => {
-      const node = photoTemplate.cloneNode();
-      node.src = photo;
-      photosBlock.appendChild(node);
-    });
-  }
-
-  photoTemplate.remove();
-
+  const avatarBlock = card.querySelector(`.popup__avatar`);
   avatarBlock.src = avatar;
   avatarBlock.alt = title;
 
